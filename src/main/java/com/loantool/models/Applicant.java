ac51d83dc557f1b71101c3aa3272c0d01627d4eb
Sithil Semitha <sithilsemithad@gmail.com>
2026-01-30 22:08:31 +0530

    compare sithil-dev with main (#7)
    * created Applicant model
    
    * add getters and setter Applicant Model
    
    * Applicant model completed
    
    * MergeSorter base setup
    
    ---------
    
    Co-authored-by: Madhuka Malshan <167554333+madhuka2002@users.noreply.github.com>

---
+++ src/main/java/com/loantool/models/Applicant.java
@@ -0,0 +1,86 @@
+package com.loantool.models;
+
+public class Applicant implements Comparable<Applicant> {
+    private String id;
+    private String name;
+    private double salary;
+    private double loanAmount;
+    private int creditScore;
+    private boolean eligible;
+
+    public Applicant(String id, String name, double salary, double loanAmount, int creditScore) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.loanAmount = loanAmount;
+        this.creditScore = creditScore;
+        this.eligible = false;
+    }
+
+    @Override
+    public int compareTo(Applicant other) {
+        // Compare by credit score in descending order
+        return Integer.compare(other.creditScore, this.creditScore);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public boolean isEligible() {
+        return eligible;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public void setEligible(boolean eligible) {
+        this.eligible = eligible;
+    }
+
+    @Override
+    public String toString() {
+        return "Applicant{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", loanAmount=" + loanAmount +
+                ", creditScore=" + creditScore +
+                ", eligible=" + eligible +
+                '}';
+    }
+
+}
